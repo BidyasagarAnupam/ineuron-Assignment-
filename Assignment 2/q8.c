f5ec917054d2a92474ca045bde8f50c3e7d51b47
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-14 16:49:12 +0530

    Solved some doubt questions

--- Assignment 2/q8.c
+++ Assignment 2/q8.c
@@ -2,5 +2,13 @@
 #include<stdio.h>
 int main() {
     //  doubt
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    if(num&1 == 1) {
+        printf("%d is an odd number", num);
+    } else {
+        printf("%d is an even number", num);
+    }
     return 0;
 }
--- Assignment 3/q4.c
+++ Assignment 3/q4.c
@@ -2,15 +2,17 @@
 Write a program to check whether a given number is an even number or an odd
 number without using % operator.
 */
-#include <stdio.h>
-int main()
-{
-    int a;
+#include<stdio.h>
+int main() {
+    //  doubt
+    int num;
     printf("Enter a number: ");
-    scanf("%d", &a);
-    // TODO Doubt...
-    // if ()
-    // {
-    // }
+    scanf("%d", &num);
+    if (num & 1 == 1) {
+        printf("%d is an odd number", num);
+    }
+    else {
+        printf("%d is an even number", num);
+    }
     return 0;
 }
