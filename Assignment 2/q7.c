510881e3db8f6e12672ad97f37236ed0590bcb40
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-14 22:40:18 +0530

    Solved doubt question

--- Assignment 2/q7.c
+++ Assignment 2/q7.c
@@ -1,6 +1,18 @@
 // Write a program to find the position of first 1 in LSB.
 #include<stdio.h>
 int main() {
-// doubt
+    // doubt
+    int num, count = 0, result = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    while (num != 0) {
+        result = num & 1;
+        count++;
+        if (result == 1) {
+            printf("%d", count);
+            break;
+        }
+        num=num>>1;
+    }
     return 0;
 }
