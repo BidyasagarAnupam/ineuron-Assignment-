6498c81a8119b3ee067ae023216e324571745799
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-16 15:09:07 +0530

    After doubt solved

--- Assignment 2/q13.c
+++ Assignment 2/q13.c
@@ -7,6 +7,7 @@ int main() {
     int a;
     printf("Enter a three digit number:  ");
     scanf("%d", &a);
-    // TODO doubt
+    // TODO doubt solved
+    printf("%d", (a % 10) * 100 + (a / 10));
     return 0;
 }
