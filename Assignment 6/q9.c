f60b9379a4284951539b118eacaad9e0e03f43c9
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-22 16:45:27 +0530

    After clearing the doubt LCM

--- Assignment 6/q9.c
+++ Assignment 6/q9.c
@@ -1,8 +1,7 @@
 // 9. Write a program to calculate LCM of two numbers
 #include<stdio.h>
 int main() {
-    int num1, num2, temp;
-    int lcm = 1;
+    int num1, num2, i;
     printf("Enter two number: ");
     scanf("%d %d", &num1, &num2);
     if (num1 % num2 == 0) {
@@ -10,19 +9,13 @@ int main() {
     }
     else if (num2 % num1 == 0) {
         printf("LCM of %d and %d is %d", num1, num2, num2);
-    }
-    else {
-        temp = num1 > num2 ? num1 : num2;
-        for (int i = 2; i <= temp; i++) {
-            if (num1 % i == 0 && num2 % i == 0) {
-                // lcm = lcm * i;
-                // num1 = num1 / i;
-                // num2 = num2 / i;
-                // temp = num1 > num2 ? num1 : num2;
-                // TODO Doubt
+    }  else {
+        for(i = 1; i<= num1*num2; i++) {
+            if(i%num1==0 && i%num2==0) {
+                break;
             }
         }
-        printf("LCM of %d and %d is %d", num1, num2, lcm);
+        printf("LCM of %d and %d is %d", num1, num2, i);
     }
     return 0;
 }
