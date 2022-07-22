70e5abb026627ca667cf8f2f68c15c4e90e9409e
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-22 17:33:08 +0530

    After clearing doubt HCF and Co-prime

--- Assignment 7/q4.c
+++ Assignment 7/q4.c
@@ -1,9 +1,16 @@
 // 4. Write a program to calculate HCF of two numbers
 #include<stdio.h>
 int main() {
-    int num1,num2;
+    int num1, num2, i, hcf;
     printf("Enter two number: ");
     scanf("%d %d", &num1, &num2);
-    // TODO DOUBT....
-return 0;
+    int min = num1 < num2 ? num1 : num2;
+    for (i = min; i >=  1; i--) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            hcf = i;
+            break;
+        }
+    }
+    printf("HCF of %d and %d is %d", num1, num2, hcf);
+    return 0;
 }
--- Assignment 7/q5.c
+++ Assignment 7/q5.c
@@ -2,9 +2,20 @@
 // numbers or not
 #include<stdio.h>
 int main() {
-    int num1,num2;
+    int num1, num2, i, hcf;
     printf("Enter two number: ");
     scanf("%d %d", &num1, &num2);
-    // TODO DOUBT....
-return 0;
+    int min = num1 < num2 ? num1 : num2;
+    for (i = min; i >= 1; i--) {
+        if (num1 % i == 0 && num2 % i == 0) {
+            hcf = i;
+            break;
+        }
+    }
+    if(hcf == 1) {
+        printf("%d and %d are Co-Prime number", num1, num2);
+    } else {
+        printf("%d and %d are not Co-Prime number", num1, num2);
+    }
+    return 0;
 }
