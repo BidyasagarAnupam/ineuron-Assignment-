f7b711de874ab1035962859f8f6e0c2a8bdb9f05
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-24 14:04:00 +0530

    After correction of yeap year question

--- Assignment 9/q6.c
+++ Assignment 9/q6.c
@@ -2,19 +2,33 @@
 // statement
 #include<stdio.h>
 int main() {
-    int year, temp;
+    int year;
     printf("Enter a Year: ");
     scanf("%d", &year);
-    temp = year % 4;
-    switch (temp)
-    {
+    switch (year % 100 == 0) {
+    case 1:
+        //  For century leaf year
+        switch (year % 400 == 0)
+        {
+        case 1:
+            printf("%d is a leap year", year);
+            break;
+        case 0:
+            printf("%d is not a leaf year", year);
+            break;
+        }
+        break;
+        //  For non-century leaf year
     case 0:
-        printf("%d is a Leap Year", year);
-        break;
-
-    default:
-        printf("%d is not a Leap Year", year);
-        break;
+        switch (year % 4 == 0)
+        {
+        case 1:
+            printf("%d is a leaf year", year);
+            break;
+        case 0:
+            printf("%d is not a leaf year", year);
+            break;
+        }
     }
     return 0;
 }
