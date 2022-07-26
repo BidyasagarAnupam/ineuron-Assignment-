d828b8927ebab8a89ec89b26641c0c4fafd2058c
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-07-26 15:37:47 +0530

    After one step of solving, but not completed

--- Assignment 11/q8.c
+++ Assignment 11/q8.c
@@ -1,2 +1,49 @@
 // 8. Write a function to print PASCAL Triangle. (TSRN)
-// TODO
+/*
+                1
+            1       1
+        1       2       1
+    1       3       3       1
+
+*/
+#include<stdio.h>
+
+//  Function declaration
+void pascalTriangle(int);
+
+// main function
+int main() {
+    int num;
+    printf("Enter the row of pascal triangle: ");
+    scanf("%d", &num);
+    pascalTriangle(num);
+    return 0;
+}
+
+//  Pascal Triangle Function
+void pascalTriangle(int row) {
+    int k = row + 1, l = k, count = 1;
+    int pascalNumber = 1; //TODO...................................         .........      .........   
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j <= row + (row - 1); j++) {
+            if (j >= k - i && j <= l - i) {
+                if (count)
+                {
+                    printf("* ");
+                    count = 0;
+                }
+                else
+                {
+                    printf("  ");
+                    count = 1;
+                }
+            }
+            else {
+                printf("  ");
+                count = 1;
+            }
+        }
+        l = l + 2;
+        printf("\n");
+    }
+}
