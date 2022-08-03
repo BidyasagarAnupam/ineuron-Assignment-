6a2a300a0509c4ad5ad02456edc89a938d8ee975
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-08-03 12:56:36 +0530

    After Solving the doubt

--- Assignment 15/q5.c
+++ Assignment 15/q5.c
@@ -2,5 +2,27 @@
 #include<stdio.h>
 
 //  function Declaration
-int fstDuplicateValue(int*);
-// TODO.....
+int fstDuplicateValue(int*, int);
+
+// main function
+int main() {
+    int size;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int arr[size];
+    printf("The first occurrence of adjacent duplicate values in the array is %d", fstDuplicateValue(arr, size));
+    return 0;
+}
+
+// function defination
+int fstDuplicateValue(int arr[], int size) {
+    printf("Enter %d numbers: ", size);
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] == arr[i + 1]) {
+            return arr[i];
+        }
+    }
+}
