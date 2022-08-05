453c03c17a8d57ec72930ee6773bd41556480bdd
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-08-05 23:48:02 +0530

    Change the approach of the solution VVI....

--- Assignment 14/q7.c
+++ Assignment 14/q7.c
@@ -2,31 +2,41 @@
 #include<stdio.h>
 
 int main() {
-    int n = 10;
-    int arr[10];
+    int n = 5;
+    int arr[n];
 
     printf("Enter %d numbers: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    //  Code for Sorting the array
-    for (int j = 0; j < n - 1; j++) {
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                continue;
-            }
-            else {
-                // Swapping
-                arr[i] = arr[i] + arr[i + 1];
-                arr[i + 1] = arr[i] - arr[i + 1];
-                arr[i] = arr[i] - arr[i + 1];
+    // Using Sorting the array
+    // for (int j = 0; j < n - 1; j++) {
+    //     for (int i = 0; i < n - 1; i++) {
+    //         if (arr[i] < arr[i + 1]) {
+    //             continue;
+    //         }
+    //         else {
+    //             // Swapping
+    //             arr[i] = arr[i] + arr[i + 1];
+    //             arr[i + 1] = arr[i] - arr[i + 1];
+    //             arr[i] = arr[i] - arr[i + 1];
+    //         }
+    //     }
+    // }
+    // printf("The second largest number is %d", arr[n-2]);
 
-            }
+//  Using another batter approach
+    int largest = 0, secondLargest = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if(arr[i] > secondLargest && largest != arr[i]) {
+            secondLargest = arr[i];
         }
     }
-
-    printf("The second largest number is %d", arr[n-2]);
+    printf("The second largest number is %d", secondLargest);
 
     return 0;
 }
--- Assignment 14/q8.c
+++ Assignment 14/q8.c
@@ -2,31 +2,40 @@
 #include<stdio.h>
 
 int main() {
-    int n = 10;
-    int arr[10];
+    int n = 5;
+    int arr[n];
 
     printf("Enter %d numbers: ", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    //  Code for Sorting the array
-    for (int j = 0; j < n - 1; j++) {
-        for (int i = 0; i < n - 1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                continue;
-            }
-            else {
-                // Swapping
-                arr[i] = arr[i] + arr[i + 1];
-                arr[i + 1] = arr[i] - arr[i + 1];
-                arr[i] = arr[i] - arr[i + 1];
+    //  Using Sorting the array
+    // for (int j = 0; j < n - 1; j++) {
+    //     for (int i = 0; i < n - 1; i++) {
+    //         if (arr[i] < arr[i + 1]) {
+    //             continue;
+    //         }
+    //         else {
+    //             // Swapping
+    //             arr[i] = arr[i] + arr[i + 1];
+    //             arr[i + 1] = arr[i] - arr[i + 1];
+    //             arr[i] = arr[i] - arr[i + 1];
+    //         }
+    //     }
+    // }
+    // printf("The second smallest number is %d", arr[1]);
 
-            }
+//  Using another batter approach
+    int smallest = 999999999, secondSmallest = 999999999;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < smallest) {
+            secondSmallest = smallest;
+            smallest = arr[i];
+        } else if( arr[i] < secondSmallest && smallest != arr[i]) {
+            secondSmallest = arr[i];
         }
     }
-
-    printf("The second smallest number is %d", arr[1]);
-
+    printf("The second smallest number is %d", secondSmallest);
     return 0;
 }
