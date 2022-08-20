f868574ef911a5f47368230aa6f4b5b1f0a11e25
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-08-20 14:39:59 +0530

    Update code

--- Assignment 19/q2.c
+++ Assignment 19/q2.c
@@ -1,15 +1,27 @@
 // Write a program to sort 10 city names stored in two dimensional arrays, taken from the user.
 #include<stdio.h>
+#include<string.h>
 
 int main() {
-    char str[10][20];
+    char str[10][20], temp[20];
 
     // user input
-    printf("Enter 10 cities name: ");
-    for (int i = 0; i < 10; i++)
-        fgets(str[i], 20, stdin);
+    printf("Enter 3 cities name: ");
+    for (int i = 0; i < 3; i++)
+        gets(str[i]);
+
+    //  logic for sort
+    for(int i=0; i<2; i++) {
+        for(int j = i+1; j<3; j++) {
+            if(strcmp(str[i], str[j]) > 0 ) {
+                strcpy(temp, str[i]);
+                strcpy(str[i], str[j]);
+                strcpy(str[j], temp);
+            }
+        }
+    }
     printf("\n");
-    for (int i = 0; i < 10; i++)
-        printf("%s", str[i]);
+    for (int i = 0; i < 3; i++)
+        printf("%s \n", str[i]);
     return 0;
 }
