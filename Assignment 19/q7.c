4f01eb1a2eaa3add5e9052576ca0e6eab242a979
Bidyasagar Mohapatra <bidyasagarofficial123@gmail.com>
2022-08-20 16:45:41 +0530

    solve the doubt question

--- Assignment 19/q7.c
+++ Assignment 19/q7.c
@@ -4,28 +4,40 @@
 56.32.78.32
 */
 #include<stdio.h>
+#include<string.h>
+#include <stdlib.h>
 
 //  function declaration
-int checkValidIp(char*);
+void checkValidIp(char*);
 
 //  main function
 int main() {
-
+    char ip[15];
+    printf("Enter a IP address\n");
+    gets(ip);
+    checkValidIp(ip);
     return 0;
 }
 
 //  function defination
-int checkValidIp(char str[]) {
-    int check = 1, noOfDots = 0;
-
-    for (int i = 0; str[i] != '\n'; i++) {
-        if (str[i] == '.')
-            noOfDots++;
+void checkValidIp(char ip[]) {
+    char temp[15];
+    strcpy(temp, ip);
+    int flag = 1, count=0;
+    char* a = strtok(ip, "."); // strtok is a library function,breaks string str into a series of tokens using the delimiter delim. 
+    while (a != NULL) {
+        int x = atoi(a); // atoi is a library functino in stdlib.h used to convert ASCII to int
+        if (!(x >= 0 && x <= 255)) {
+            flag = 0;
+            break;
+        } else {
+            count++;
+        }
+        a = strtok(NULL, ".");
     }
-    if(noOfDots != 3) {
-        check = 0;
-    } else {
-        // TODO Doubt..................
-    }
+    if (flag && count == 4)
+        printf("%s is an valid IP address", temp);
+    else
+        printf("%s is not an valid IP address", temp);
 
 }
--- Assignment 19/q8.c
+++ Assignment 19/q8.c
@@ -1,3 +1,35 @@
 /*
 Given a list of words followed by two words, the task is to find the minimum distance between the given two words in the list of words. (Example : s = {“the”,”quick”,”brown”,”fox”,”quick”} word1 = “the”, word2 = “fox”, OUTPUT : 1 )
 */
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int main() {
+    char words[5][20] = {"the", "quick", "brown", "fox", "quick"};
+    char word1[20], word2[20];
+    int w1 = -1, w2 =-1, min = 100000, temp;
+
+    for(int i=0; i<5; i++) {
+        printf("\"%s\" ", words[i]);
+    }
+    printf("\nEnter a word from above \n");
+    gets(word1);
+    printf("Enter an another word from above \n");
+    gets(word2);
+
+    for(int i=0; i<5; i++) {
+        if(strcmp(words[i], word1) == 0) {
+            w1 = i;
+        }
+        if(strcmp(words[i], word2) == 0) {
+            w2 = i;
+        }
+        if(w1 != -1 && w2 != -1) {
+            temp = abs(w2-w1);
+            if(temp < min) 
+                min = temp;
+        }
+    }
+    printf("The minimum length between %s and %s is %d", word1, word2, min-1);
+}
